0553788db0e432980d0ab77038f8913f9561b1ac
Manel Clos <manelclos@gmail.com>
2020-11-12 17:45:10 +0100

    v114

--- django_quasar/__init__.py
+++ django_quasar/__init__.py
@@ -2,3 +2,6 @@ from django.conf import settings
 
 CSS_QUASAR_111 = 'js/quasar-1.11.3/quasar%s.css' % ('' if not settings.DEBUG else '.min')
 JS_QUASAR_111 = 'js/quasar-1.11.3/quasar.umd%s.js' % ('' if not settings.DEBUG else '.min')
+
+CSS_QUASAR_114 = 'js/quasar-1.14.3/quasar%s.css' % ('' if not settings.DEBUG else '.min')
+JS_QUASAR_114 = 'js/quasar-1.14.3/quasar.umd%s.js' % ('' if not settings.DEBUG else '.min')
