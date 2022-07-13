bff1f0f830225a9a1b86864189d480128c025e22
MartiColl <mcollrisco25@gmail.com>
2022-07-13 11:37:44 +0200

    actualització versió quasar

--- django_quasar/__init__.py
+++ django_quasar/__init__.py
@@ -1,4 +1,7 @@
 from django.conf import settings
 
-CSS_QUASAR_111 = 'js/quasar-1.11.3/quasar%s.css' % ('' if not settings.DEBUG else '.min')
-JS_QUASAR_111 = 'js/quasar-1.11.3/quasar.umd%s.js' % ('' if not settings.DEBUG else '.min')
+CSS_QUASAR_111 = 'js/quasar-1.11.3/quasar%s.css' % ('' if settings.DEBUG else '.min')
+JS_QUASAR_111 = 'js/quasar-1.11.3/quasar.umd%s.js' % ('' if settings.DEBUG else '.min')
+
+CSS_QUASAR_27 = 'js/quasar-2.7.5/quasar%s.css' % ('' if settings.DEBUG else '.prod')
+JS_QUASAR_27 = 'js/quasar-2.7.5/quasar.umd%s.js' % ('' if settings.DEBUG else '.prod')
--- django_quasar/mixins.py
+++ django_quasar/mixins.py
@@ -1,7 +1,6 @@
 from django import forms
 
-from . import CSS_QUASAR_111, JS_QUASAR_111
-
+from . import CSS_QUASAR_111, JS_QUASAR_111, CSS_QUASAR_27, JS_QUASAR_27
 
 class Quasar_111_Mixin(object):
 
@@ -13,6 +12,16 @@ class Quasar_111_Mixin(object):
         js = [JS_QUASAR_111]
         return super().media + forms.Media(js=js, css=css)
 
+class Quasar_27_Mixin(object):
 
-class QuasarMixin(Quasar_111_Mixin):
+    @property
+    def media(self):
+        css = {
+            'all': (CSS_QUASAR_27,)
+        }
+        js = [JS_QUASAR_27]
+        return super().media + forms.Media(js=js, css=css)
+
+
+class QuasarMixin(Quasar_27_Mixin):
     pass
